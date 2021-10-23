fa0affac3e03675727daee7c6a2e5b3232fb5aec
hyeonmin <hyeonmin97@naver.com>
2021-10-23 17:53:16 +0900

    다이얼로그 안꺼지게 변경

--- app/src/main/java/kr/ac/dongyang/project/LoadingDialog.java
+++ app/src/main/java/kr/ac/dongyang/project/LoadingDialog.java
@@ -19,7 +19,7 @@ public class LoadingDialog extends Dialog {
 
     public LoadingDialog(@NonNull Context context) {
         super(context);
-
+        this.setCancelable(false);
         requestWindowFeature(Window.FEATURE_NO_TITLE);   //다이얼로그의 타이틀바를 없애주는 옵션입니다.
         Objects.requireNonNull(getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));  //다이얼로그의 배경을 투명으로 만듭니다.
         getWindow().setGravity(Gravity.CENTER);
